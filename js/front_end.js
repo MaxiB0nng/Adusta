b6b284d15a9b2bb042493fb120dfb8579874b773
gunterdk <malthebregendahl@hotmail.com>
2025-01-22 10:25:53 +0100

    Changed Charge and Shoot buttons for player 2

--- js/front_end.js
+++ js/front_end.js
@@ -1,7 +1,7 @@
 document.addEventListener('keydown', function(e) {
     const keys = {
         'w':'p1-up', 'a':'p1-left', 's':'p1-down', 'd':'p1-right', 'q':'p1-charge', 'e':'p1-shoot',
-        'i':'p2-up', 'j':'p2-left', 'k':'p2-down', 'l':'p2-right', 'u':'p2-charge', 'o':'p2-shoot'
+        'i':'p2-up', 'j':'p2-left', 'k':'p2-down', 'l':'p2-right', 'o':'p2-charge', 'u':'p2-shoot'
     };
     if (keys[e.key]) {
         document.querySelector(`.${keys[e.key]}`).classList.add('key-press');
@@ -11,7 +11,7 @@ document.addEventListener('keydown', function(e) {
 document.addEventListener('keyup', function(e) {
     const keys = {
         'w':'p1-up', 'a':'p1-left', 's':'p1-down', 'd':'p1-right', 'q':'p1-charge', 'e':'p1-shoot',
-        'i':'p2-up', 'j':'p2-left', 'k':'p2-down', 'l':'p2-right', 'u':'p2-charge', 'o':'p2-shoot'
+        'i':'p2-up', 'j':'p2-left', 'k':'p2-down', 'l':'p2-right', 'o':'p2-charge', 'u':'p2-shoot'
     };
     if (keys[e.key]) {
         document.querySelector(`.${keys[e.key]}`).classList.remove('key-press');
